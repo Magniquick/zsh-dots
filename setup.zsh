b5f8a56495910353360977e53928f34f561dcb81
Magniquick <67406856+Magniquick@users.noreply.github.com>
2026-04-14 05:26:11 +0530

    fix: rebuild waits for bg jobs, compile smartcache + colors

--- setup.zsh
+++ setup.zsh
@@ -43,25 +43,31 @@ function rebuild() {
 	echo "starting compile"
 	# Compile Powerlevel10k package
 	make -sC $ZPLUGDIR/powerlevel10k pkg zwc &
-	zcompile-many $ZDOTDIR/.p10k.zsh &
+	zcompile-many $ZDOTDIR/.p10k.zsh $ZDOTDIR/.p10k.colors.zsh &
 	zcompile-many $XDG_CACHE_HOME/p10k-instant-prompt-*.zsh &
-	
+
 	# Compile Fast Syntax Highlighting scripts
 	zcompile-many $ZPLUGDIR/fast-syntax-highlighting/{fast*,.fast*,**/*.ch,**/*.zsh} &
-	
+
 	# Compile Zsh Autosuggestions scripts
 	zcompile-many $ZPLUGDIR/zsh-autosuggestions/{*.zsh,src/**/*.zsh} &
-	
+
 	# fzf-tab and friends.
 	zcompile-many $ZPLUGDIR/zsh-completions/{*.zsh,src/*} &
 	zcompile-many $ZPLUGDIR/fzf-tab/*.zsh &
 	zcompile-many $ZPLUGDIR/fzf-tab-source/{*.zsh,sources/*.zsh,functions/*.zsh} &
-	
+
+	# smartcache
+	zcompile-many $ZPLUGDIR/zsh-smartcache/*.zsh &
+
 	#compinit
 	zcompile-many $XDG_CACHE_HOME/zsh/^(*.(zwc|old)) &
-	
+
 	#and some more files too
 	zcompile-many $ZDOTDIR/{*.zsh,.zprofile,.zshrc} &
+
+	wait
+	echo "compile done"
 }
 
 check_optional_dependencies
@@ -74,5 +80,4 @@ if [[ ! -e $XDG_CONFIG_HOME/fsh/catppuccin-mocha.ini ]]; then
 	curl --create-dirs -O --output-dir $XDG_CONFIG_HOME/fsh https://raw.githubusercontent.com/catppuccin/zsh-fsh/main/themes/catppuccin-mocha.ini # catppucin gang rise up !
 	fast-theme XDG:catppuccin-mocha
 fi
-wait # wait for all background jobs to finish
 popd
